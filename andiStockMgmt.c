e4912fedeb9f42c1f61b5056d85207481df954a6
fadhil daeng naba <69493052+fdaengnaba@users.noreply.github.com>
2023-03-10 18:36:38 +0700

    add menu description

--- andiStockMgmt.c
+++ andiStockMgmt.c
@@ -15,6 +15,12 @@ typedef struct node {
     struct node *next;
 } Node;
 
+/*Tambah barang: menu ini digunakan untuk menambahkan barang baru ke dalam stok barang. */
+/* Hapus barang: menu ini digunakan untuk menghapus barang yang sudah tidak diperlukan dari stok barang. */
+/* Tampilkan stok barang: menu ini digunakan untuk menampilkan daftar barang yang ada di dalam stok barang beserta jumlah stoknya. */
+/* Cari barang: menu ini digunakan untuk mencari sebuah barang di dalam stok barang berdasarkan nama barang. */
+/* Update stok barang: menu ini digunakan untuk mengupdate stok barang jika terjadi penjualan atau pembelian barang.*/
+
 
 int main() {
     Node *head = NULL;
